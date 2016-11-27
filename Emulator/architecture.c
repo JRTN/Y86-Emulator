e2a8c142a88e9900e1def01946d31789e0cf0efc
John Russell <john@JJRDebianVM>
2016-11-26 19:51:40 -0600

    Execute loop working, shell for instruction functions created

--- Emulator/architecture.c
+++ Emulator/architecture.c
@@ -1,14 +1,47 @@
-#include "architecture.h"
-#include "util.h"
 #include <string.h>
 #include <malloc.h>
 #include <ctype.h>
 
+#include "architecture.h"
+#include "util.h"
+
 static cpu_t cpu;
 static char *memory;
 static int32_t size;
-
 static status_t status = AOK;
+
+static void halt(void);
+static void rrmovl(void);
+static void irmovl(void);
+static void rmmovl(void);
+static void mrmovl(void);
+
+static void addl(void);
+static void subl(void);
+static void andl(void);
+static void xorl(void);
+static void mull(void);
+static void cmpl(void);
+
+static void jmp(void);
+static void jle(void);
+static void jne(void);
+static void jge(void);
+static void jg(void);
+static void jl(void);
+static void je(void);
+
+static void call(void);
+static void ret(void);
+
+static void pushl(void);
+static void popl(void);
+static void readb(void);
+static void readl(void);
+static void writeb(void);
+static void writel(void);
+
+static void movsbl(void);
 
 /*
     Executes the instructions stored in memory until the status of the machine
@@ -20,8 +53,9 @@ static status_t status = AOK;
         The status of the machine when it stops.
 */
 status_t execute() {
+    char *instruction = calloc(3, sizeof(char));
+    instruction[2] = '\0';
     while(status == AOK) {
-        char* instruction = malloc(3);
         strncpy(instruction, memory + cpu.ipointer, 2);
         int32_t n_instruction = hexToDec(instruction);
         printf("%x\n", n_instruction);
@@ -32,111 +66,137 @@ status_t execute() {
             break;
             case 0x10: /* halt */
                 printf("halt\n");
-                status = HLT;
+                halt();
                 cpu.ipointer += 1 BYTE;
             break;
             case 0x20: /* rrmovl */
                 printf("rrmovl\n");
+                rrmovl();
                 cpu.ipointer += 2 BYTE;
             break;
             case 0x30: /* irmovl */
                 printf("irmovl\n");
+                irmovl();
                 cpu.ipointer += 6 BYTE;
             break;
             case 0x40: /* rmmovl */
                 printf("rmmovl\n");
+                rmmovl();
                 cpu.ipointer += 6 BYTE;
             break;
             case 0x50: /* mrmovl */
                 printf("mrmovl\n");
+                mrmovl();
                 cpu.ipointer += 6 BYTE;
             break;
             case 0x60: /* addl */
                 printf("addl\n");
+                addl();
                 cpu.ipointer += 2 BYTE;
             break;
             case 0x61: /* subl */
                 printf("subl\n");
+                subl();
                 cpu.ipointer += 2 BYTE;
             break;
             case 0x62: /* andl */
                 printf("andl\n");
+                andl();
                 cpu.ipointer += 2 BYTE;
             break;
             case 0x63: /* xorl */
                 printf("xorl\n");
+                xorl();
                 cpu.ipointer += 2 BYTE;
             break;
             case 0x64: /* mull */
                 printf("mull\n");
+                mull();
                 cpu.ipointer += 2 BYTE;
             break;
             case 0x65: /* cmpl */
                 printf("cmpl\n");
+                cmpl();
                 cpu.ipointer += 2 BYTE;
             break;
             case 0x70: /* jmp */
                 printf("jmp\n");
+                jmp();
                 cpu.ipointer += 5 BYTE;
             break;
             case 0x71: /* jle */
                 printf("jle\n");
+                jle();
                 cpu.ipointer += 5 BYTE;
             break;
             case 0x72: /* jl */
                 printf("jl\n");
+                jl();
                 cpu.ipointer += 5 BYTE;
             break;
             case 0x73: /* je */
                 printf("je\n");
+                je();
                 cpu.ipointer += 5 BYTE;
             break;
             case 0x74: /* jne */
                 printf("jne\n");
+                jne();
                 cpu.ipointer += 5 BYTE;
             break;
             case 0x75: /* jge */
                 printf("jge\n");
+                jge();
                 cpu.ipointer += 5 BYTE;
             break;
             case 0x76: /* jg */
                 printf("jg\n");
+                jg();
                 cpu.ipointer += 5 BYTE;
             break;
             case 0x80: /* call */
                 printf("call\n");
+                call();
                 cpu.ipointer += 5 BYTE;
             break;
             case 0x90: /* ret */
                 printf("ret\n");
+                ret();
                 cpu.ipointer += 1 BYTE;
             break;
             case 0xA0: /* pushl */
                 printf("pushl\n");
+                pushl();
                 cpu.ipointer += 2 BYTE;
             break;
             case 0xB0: /* popl */
                 printf("popl\n");
+                popl();
                 cpu.ipointer += 2 BYTE;                
             break;
             case 0xC0: /* readb */
                 printf("readb\n");
+                readb();
                 cpu.ipointer += 6 BYTE;
             break;
             case 0xC1: /* readl */
                 printf("readl\n");
+                readl();
                 cpu.ipointer += 6 BYTE;
             break;
             case 0xD0: /* writeb */
                 printf("writeb\n");
+                writeb();
                 cpu.ipointer += 6 BYTE;
             break;
             case 0xD1: /* writel */
                 printf("writel\n");
+                writel();
                 cpu.ipointer += 6 BYTE;
             break;
             case 0xE0: /* movsbl */
                 printf("movsbl\n");
+                movsbl();
                 cpu.ipointer += 6 BYTE;
             break;
             default:
@@ -144,6 +204,7 @@ status_t execute() {
                 printf("Unknown Instruction Encountered\n");
         }
     }
+    free(instruction);
     return status;
 }
 
@@ -281,4 +342,112 @@ void printMemory() {
             printf("-");
         }
     }
+}
+
+static void halt() {
+    status = HLT;
+}
+
+static void rrmovl() {
+
+}
+
+static void irmovl() {
+
+}
+
+static void rmmovl() {
+
+}
+
+static void mrmovl() {
+
+}
+
+static void addl() {
+
+}
+
+static void subl() {
+
+}
+
+static void andl() {
+
+}
+
+static void xorl() {
+
+}
+
+static void mull() {
+
+}
+
+static void cmpl() {
+
+}
+
+static void jmp() {
+
+}
+
+static void jle() {
+
+}
+
+static void jne() {
+
+}
+
+static void jge() {
+
+}
+
+static void jg() {
+
+}
+
+static void jl() {
+
+}
+
+static void je() {
+
+}
+
+static void call() {
+
+}
+
+static void ret() {
+
+}
+
+static void pushl() {
+
+}
+
+static void popl() {
+
+}
+
+static void readb() {
+
+}
+
+static void readl() {
+
+}
+
+static void writeb() {
+
+}
+
+static void writel() {
+
+}
+
+static void movsbl() {
+
 }
--- Emulator/architecture.h
+++ Emulator/architecture.h
@@ -22,18 +22,9 @@ typedef enum status_e {
     AOK, HLT, ADR, INS
 } status_t;
 
-typedef union ByteParts_u {
-    struct {
-        unsigned int first : 4;
-        unsigned int second : 4;
-    } halves;
-    char c;
-} ByteParts_t;
-
 typedef struct cpu_s {
     reg_t registers[NUM_REGISTERS];
     int32_t ipointer;
-
     int8_t OF;
     int8_t SF;
     int8_t ZF;
@@ -50,41 +41,5 @@ int putByte(char, int32_t);
 int putLong(int32_t, int32_t);
 int bss(int32_t, int32_t);
 
-void halt(void);
-void rrmovl(reg_t, reg_t);
-void irmovl(int32_t, reg_t);
-void rmmovl(reg_t, reg_t, int32_t);
-void mrmovl(reg_t, reg_t, int32_t);
-
-void addl(reg_t, reg_t);
-void subl(reg_t, reg_t);
-void andl(reg_t, reg_t);
-void xorl(reg_t, reg_t);
-void mull(reg_t, reg_t);
-void cmpl(reg_t, reg_t);
-
-void jmp(int32_t);
-void jle(int32_t);
-void jne(int32_t);
-void jge(int32_t);
-void jg(int32_t);
-void jl(int32_t);
-void je(int32_t);
-
-void call(int32_t);
-
-void ret(void);
-
-void pushl(reg_t);
-
-void popl(reg_t);
-
-void readb(reg_t, int32_t);
-void readl(reg_t, int32_t);
-
-void writeb(reg_t, int32_t);
-void writel(reg_t, int32_t);
-
-void movsbl(reg_t, reg_t, int32_t);
 
 #endif
