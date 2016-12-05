dde9c7fb2129f4747abe69a7f82488c4bd7ca61d
John Russell <john@JJRDebianVM>
2016-12-05 14:55:37 -0600

    Final (hopefully) sync

--- Emulator/y86emul.c
+++ Emulator/y86emul.c
@@ -2,10 +2,16 @@
 #include "architecture.h"
 
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char **argv) {
     if(argc < 2) {
         fprintf(stderr, "ERROR: Must have at least one argument\n");
+        return 1;
+    }
+    if(strcmp("-h", argv[1]) == 0) {
+        printf("Usage: y86emul <inputfile>\n");
+        return 0;
     }
 	if(!loadFileIntoMemory(argv[1])) {
         return 1;
